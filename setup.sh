3a720b907e9489d062e3673f057e461d1ce6c8a7
Jimmy Carter <148771412+SSN-23@users.noreply.github.com>
2025-04-08 07:45:27 +0000

    Updated setup.sh again!!

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@
 
 sudo pacman -Sy --noconfirm awesome picom
 
-mkdir ~/.configs/picom
+mkdir ~/.config/picom
 
 git clone https://github.com/SSN-23/configs.git
-sudo cp ~/configs/picom.conf ~/.configs/picom/
+sudo cp ~/configs/picom.conf ~/.config/picom/
 sudo cp ~/configs/picom.service /etc/systemd/system/
 
 ln -sf ~/configs/.bashrc ~/.bashrc
 ln -sf ~/configs/.xinitrc ~/.xinitrc
-ln -sf ~/configs/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -sf ~/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -sf ~/configs/picom/picom.conf ~/.config/picom/picom.conf
 ln -sf ~/configs/picom.service ~/etc/systemd/system/picom.service
 
