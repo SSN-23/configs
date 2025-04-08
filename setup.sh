53a3fcb774bcf8a8228255a1b94082a4055d7961
Jimmy Carter <148771412+SSN-23@users.noreply.github.com>
2025-04-08 08:25:36 +0000

    Removed a bug

--- setup.sh
+++ setup.sh
@@ -3,16 +3,12 @@
 sudo pacman -Sy --noconfirm awesome picom
 
 mkdir ~/.config/picom
-
-git clone https://github.com/SSN-23/configs.git
 sudo cp ~/configs/picom.conf ~/.config/picom/
 sudo cp ~/configs/picom.service /etc/systemd/system/
 
 ln -sf ~/configs/.bashrc ~/.bashrc
 ln -sf ~/configs/.xinitrc ~/.xinitrc
 ln -sf ~/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
-ln -sf ~/configs/picom/picom.conf ~/.config/picom/picom.conf
-ln -sf ~/configs/picom.service ~/etc/systemd/system/picom.service
 
 sudo systemctl enable picom.service
 sudo systemctl daemon-reload
