a228e6589c65868b687ed365dc1600a597848ca7
Jimmy Carter <148771412+SSN-23@users.noreply.github.com>
2025-08-18 13:37:45 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo cp ~/configs/picom.service /etc/systemd/system/
 
 ln -sf ~/configs/.bashrc ~/.bashrc
 ln -sf ~/configs/.xinitrc ~/.xinitrc
-ln -sf ~/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -sf ~/configs/lxterminal ~/.config/alacritty/lxterminal
 
 sudo systemctl enable picom.service
 sudo systemctl daemon-reload
